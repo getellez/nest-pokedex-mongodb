2c385abc1b885b46d4ac56ab78d60c1419369113
German Tellez <gertellezv@gmail.com>
2024-05-24 14:40:46 -0500

    fin seccion 8 - paginacion

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -9,6 +9,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { isValidObjectId, Model } from 'mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -27,8 +28,15 @@ export class PokemonService {
     }
   }
 
-  findAll() {
-    return this.pokemonModel.find({});
+  findAll({ limit = 10, offset = 0 }: PaginationDto) {
+    // skip: number = 5, limit: number = 5
+    console.log({ limit, offset });
+    return this.pokemonModel
+      .find({})
+      .limit(limit)
+      .skip(offset)
+      .sort({ no: 1 })
+      .select('-__v');
   }
 
   async findOne(term: string) {
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
   async runSeed() {
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=2',
-    );
+    try {
+      await this.pokemonModel.deleteMany({});
+      const data = await this.http.get<PokeResponse>(
+        'https://pokeapi.co/api/v2/pokemon?limit=650',
+      );
+      const pokemons = [];
+      data.results.forEach(({ name, url }) => {
+        const segments = url.split('/');
+        const no: number = +segments[segments.length - 2];
+        pokemons.push({ name, no });
+      });
 
-    data.results.forEach(({ name, url }) => {
-      const segments = url.split('/');
-      const no: number = +segments[segments.length - 2];
-      console.log({ name, no });
-    });
+      await this.pokemonModel.insertMany(pokemons);
 
-    return data.results;
+      return data.results;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`Pokemon already exists in db`);
+      }
+      console.error(error);
+      throw new InternalServerErrorException();
+    }
   }
 }
